a6baeb5126bcc8bbb8eff265818db2467373eeb6
fwhez <1728565484@qq.com>
2019-07-18 17:58:19 +0800

    profile readme

---
+++ examples/modules/heartbeat/README.md
@@ -0,0 +1,15 @@
+## heartbeat
+This example shows how to use official way to watch heartbeat. You can also design it yourself.
+
+#### step
+
+`cd server`
+
+`go run server.go`
+
+`cd client`
+
+`go run client.go`
+
+#### result
+server side outputs heartbeat pack info each 10s.
--- examples/modules/online-offline/server/server.go
+++ examples/modules/online-offline/server/server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/fwhezfwhez/tcpx"
 	"fmt"
 	"github.com/fwhezfwhez/errorx"
+	"github.com/fwhezfwhez/tcpx"
 	//"tcpx"
 )
 
@@ -18,20 +18,19 @@ func main() {
 }
 
 func online(c *tcpx.Context) {
-    type Login struct{
-    	Username string `json:"username"`
+	type Login struct {
+		Username string `json:"username"`
 	}
-    var login Login
-    if _, e:=c.Bind(&login);e!=nil {
-    	fmt.Println(errorx.Wrap(e).Error())
-    	return
+	var login Login
+	if _, e := c.Bind(&login); e != nil {
+		fmt.Println(errorx.Wrap(e).Error())
+		return
 	}
-    c.Online(login.Username)
+	c.Online(login.Username)
 	fmt.Println("online success")
-	c.JSON(2, "login success")
 }
 
 func offline(c *tcpx.Context) {
 	fmt.Println("offline success")
-    c.Offline()
+	c.Offline()
 }
