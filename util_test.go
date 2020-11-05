dd285d3de9d6d15fe0e26ab890861b4145a9db48
fwhez <1728565484@qq.com>
2020-11-05 14:40:22 +0800

    add testcase

--- util_test.go
+++ util_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestIn(t *testing.T) {
 	// true
-	fmt.Println(In("ipskzk",[]string{"ip%"}))
-	fmt.Println(In("ipskzk",[]string{"%ip%"}))
-	fmt.Println(In("kjlk;lip",[]string{"%ip"}))
-	fmt.Println(In("kjlk;lip",[]string{"%ip%"}))
+	fmt.Println(In("ipskzk", []string{"ip%"}))
+	fmt.Println(In("ipskzk", []string{"%ip%"}))
+	fmt.Println(In("kjlk;lip", []string{"%ip"}))
+	fmt.Println(In("kjlk;lip", []string{"%ip%"}))
 }
 
 func TestDebug(t *testing.T) {
@@ -18,11 +18,30 @@ func TestDebug(t *testing.T) {
 }
 
 func TestDefer(t *testing.T) {
-	f := func(){
+	f := func() {
 		fmt.Println(1)
 		panic(1)
 	}
-	Defer(f, func(v interface{}){
+	Defer(f, func(v interface{}) {
 		fmt.Println(v)
 	})
+}
+
+func TestMarshalToml(t *testing.T) {
+	b, e := MarshalTOML(map[string]interface{}{
+		"name": "ft",
+	})
+	if e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+	fmt.Println(string(b))
+
+	var m map[string]interface{}
+
+	if e := UnmarshalTOML(b, &m); e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+	fmt.Println(m)
 }
